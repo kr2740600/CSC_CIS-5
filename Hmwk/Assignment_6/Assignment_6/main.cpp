2bda7a39d4cccf82d73f47cf50801feeac45dbc7
kr2740600 <kylerincon@gmail.com>
2017-07-21 16:53:55 -0700

    Problem 2 for Assignment 6

--- Hmwk/Assignment_6/Assignment_6/main.cpp
+++ Hmwk/Assignment_6/Assignment_6/main.cpp
@@ -14,19 +14,32 @@ bool firstLast2(int ar[], int length){
         }
     }
 }
+
+//create function for problem 2
+int countNum2s(int arr[], int length);
+int countNum2s(int arr[],int length){
+    int counter=0;
+    for(int i=0;i<length;++i){
+        if(arr[i]==2){
+            counter+=1;
+        }
+    }
+    return counter;
+}
 int main(int argc, char** argv) {
     //declare variable for loop
     char choice;
     
     do{
-        cout<<"Type 1 for problem 1 from Savitch Array Chapter Practice Program 1"<<endl;
-        
+        cout<<"Type 1 for problem 1 from Savitch's Array Chapter Practice Program 1"<<endl;
+        cout<<"Type 2 for problem 2 from Savitch's Array Chapter Practice Program 2"<<endl;
         //get user choice
         cin>>choice;
         
         //use switch for problems
         switch(choice){
             case'1':{
+                //problem 1
                 //declare variables
                 int myAr[]={2,3,4,5,7};
                 int leng;
@@ -43,7 +56,17 @@ int main(int argc, char** argv) {
                 }else{
                     cout<<"There was not a 2 at the beginning or end of the array"<<endl;
                 }
+                break;
+            }
+            case'2':{
+                //problem 2
+                //declare variables
+                int ar2[]={2,3,5,10,19,20,2,23};
+                int num2s;
                 
+                num2s=countNum2s(ar2,8);
+                cout<<"The number of 2's in the array: "<<num2s<<endl;
+                break;
             }
             
             //end of switch
