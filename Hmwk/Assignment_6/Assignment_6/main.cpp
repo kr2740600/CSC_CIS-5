01198c5a8273875e34c90c26aa539715bc059a0e
kr2740600 <kylerincon@gmail.com>
2017-07-24 16:54:32 -0700

    Added more problems to Assignment 6

--- Hmwk/Assignment_6/Assignment_6/main.cpp
+++ Hmwk/Assignment_6/Assignment_6/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -71,16 +75,76 @@ string lookupName(string tName, string names[], string phones[],int num){
     return phonum;
 }
 
+//function for problem 5
+string delete_repeats(char array[],int asize);
+string delete_repeats(char array[],int asize){
+    int i,j;
+    for(i=0;i<asize;++i){
+        for(j=0;j<asize;++j){
+            if(j!=i){
+                if (array[j]==array[i]){
+                array[j]=' ';
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void display_5(char array[],int asize);
+void display_5(char array[],int asize){
+    int i;
+    for(i=0;i<asize;++i){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void display_sarray(string array[], int size);
+void display_sarray(string array[],int size){
+    int i;
+    for (i=0;i<size;++i){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+//problem 6 functions
+float get_average(float array[], int size);
+float get_average(float array[],int size){
+    float sum,avg;
+    for(int i=0;i<size;++i){
+        sum+=array[i];
+    }
+    avg=sum/size;
+    return avg;
+}
+
+float get_sd(float ar[],int size, float avg);
+float get_sd(float ar[],int size, float avg){
+    float sd;
+    for(int i=0;i<size;++i){
+        sd+=(pow((ar[i]-avg),2))/size;
+    }
+    return sd;
+}
+//problem 7 functions
+
+
 int main(int argc, char** argv) {
     //declare variable for loop
     char choice;
     
     do{
-        cout<<"Type 1 for problem 1 from Savitch's Array Chapter Practice Program 1"<<endl;
-        cout<<"Type 2 for problem 2 from Savitch's Array Chapter Practice Program 2"<<endl;
-        cout<<"Type 3 for problem 3 from Savitch's Array Chapter Practice Program 3"<<endl;
-        cout<<"Type 4 for problem 4 from Savitch's Array Chapter Practice Program 4"<<endl;
-        
+        cout<<"Type 1 for problem 1 from Savitch's Array Chapter Practice Program"<<endl;
+        cout<<"Type 2 for problem 2 from Savitch's Array Chapter Practice Program"<<endl;
+        cout<<"Type 3 for problem 3 from Savitch's Array Chapter Practice Program"<<endl;
+        cout<<"Type 4 for problem 4 from Savitch's Array Chapter Practice Program"<<endl;
+        cout<<"Type 5 for problem 3 from Savitch's Array Chapter Programming Project"<<endl;
+        cout<<"Type 6 for problem 4 from Savitch's Array Chapter Programming Project"<<endl;
+        cout<<"Type 7 for problem"<<endl;
+        cout<<"Type 8 for problem "<<endl;
+        cout<<"Type 9 for problem "<<endl;
+        cout<<"Type 'a' for problem "<<endl<<endl;
         
         //get user choice
         cin>>choice;
@@ -190,6 +254,112 @@ int main(int argc, char** argv) {
                 }while(c=='y');
                     break;
             }//end of problem 4
+            case '5':{
+                //problem 5
+                //prog proj 3 savitch 9thEd num 3
+                
+                //declare variables
+                char repeat[10];
+                int size;
+                string conv;
+                int k;
+                
+                //initialize variables
+                repeat[0]='a';
+                repeat[1]='b';
+                repeat[2]='a';
+                repeat[3]='c';
+                size=4;
+                
+                //display the original array
+                display_5(repeat,size);
+                //check to see if there are duplicates and delete them
+                conv=delete_repeats(repeat,size);
+                //display new array
+                for(k=0;k<size;++k){
+                    if(conv[k]!=' '){
+                    cout<<conv[k]<<" ";
+                    }
+                }
+                break;
+            }//end of problem 5
+            case '6':{
+                //problem 6
+                float average;
+                float stand;
+                float array6[15];
+                int size;
+                
+                array6[0]=1;
+                array6[1]=10;
+                array6[2]=15;
+                array6[3]=20;
+                size=4;
+                
+                //compute average
+                average=get_average(array6,size);
+                cout<<"The average is: "<<average<<endl;
+                //compute standard deviation
+                stand=get_sd(array6,size,average);
+                cout<<"The standard deviation is: "<<stand<<endl;
+                break;
+            }//end of problem 6
+            case'7':{
+                //problem 7
+                //declare variables
+                vector<int>nums;
+                int fileo;
+                int addnums;
+                char rnum;
+                string filename;
+                int copies;
+                
+                //ask the user whether they want to use a file or enter nums themselves
+                cout<<"Enter '1' to read in a file or '2' to enter numbers into the array"<<endl;
+                cin>>fileo;
+                if(fileo==1){
+                    cout<<"What is the name of the file"<<endl;
+                    getline(cin,filename);
+                } else if(fileo==2){
+                    do{
+                    cout<<"Enter in a value"<<endl;
+                    cin>>addnums;
+                    nums.push_back(addnums);
+                    cout<<"Do you want to add another number? y/n"<<endl;
+                    cin>>rnum;
+                    }while(rnum=='y');
+                }
+                //sort the numbers in descending order
+                std::sort(nums.begin(),nums.end(),std::greater<int>());
+                
+                //print the numbers
+                for(int i=0;i<nums.size();++i){
+                    for(int j=0;j<nums.size();++j){
+                        if(nums[i]==nums[j]){
+                            copies++;
+                            
+                        }
+                    }
+                    cout<<nums[i]<<"  "<<copies<<endl;
+                    copies=0;
+                }
+                break;
+            }//end of problem 7
+            case '8':{
+                //problem 8
+                
+                break;
+            }//end of problem 8
+            case '9':{
+                //problem 9
+                
+                break;
+            }//end of problem 9
+            case 'a':{
+                //problem 10
+                
+                break;
+            }//end of problem 10
             
         }//end of switch statement
         
