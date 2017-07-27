65759701cc085b6ca23db68660aeafedaaff32ba
kr2740600 <kylerincon@gmail.com>
2017-07-27 16:52:15 -0700

    Project 2.3 vectors added

--- Proj/Project_2/Project_2/main.cpp
+++ Proj/Project_2/Project_2/main.cpp
@@ -3,19 +3,47 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <vector>
 
 
 using namespace std;
 //create object
-class Player{
+/*class Player{
 public:
-    //
+    //variables
+    
+    //functions
+    Player(int die[],int score,int rounds,int games);
     int roll_the_dice();
+    void display();
+    void writeFile();
     
 private:
     //
-        
+    int dice[];
+    int points;
+    int gplayed;
+    int roundz;
+    
+      
 };
+
+//class Functions;
+Player::Player(int die[],int score,int rounds,int games):
+dice(die),points(score),roundz(rounds),gplayed(games)
+{};
+
+int Player::roll_the_dice(){
+    //create vector to store dice
+    
+    int random;
+    for(int i=0;i<9;++i){
+        srand(time(NULL));
+        dice[i]=rand()%6+1;
+        //dice[i]=random;
+    }
+    return dice;
+}*/
 
 
 //my function
@@ -30,10 +58,16 @@ void explain_rules(){
 }
 
 //create a function to roll the die from 1-6
-int roll_the_dice();
-int roll_the_dice(){
-    int dice;
-    dice=rand()%6+1;
+vector<int> roll_the_dice();
+vector<int> roll_the_dice(){
+    vector <int> dice;
+    srand(time(NULL));
+    for(int i=0;i<9;++i){
+        int random=0;
+        //srand(time(NULL));
+        random=rand()%6+1;
+        dice.push_back(random);
+    }
     return dice; 
 }
 
@@ -49,43 +83,27 @@ void write_to_file(ofstream&save, int point, int games){
 }
 
 //display the value of each roll
-void display_roll(int a, int b, int c, int d, int e, int f, int g, int h, int j);
-void display_roll(int a, int b, int c, int d, int e, int f, int g, int h, int j){
-    cout<<"Dice 1 rolled: "<<a<<endl;
-    cout<<"Dice 2 rolled: "<<b<<endl;
-    cout<<"Dice 3 rolled: "<<c<<endl;
-    cout<<"Dice 4 rolled: "<<d<<endl;
-    cout<<"Dice 5 rolled: "<<e<<endl;
-    cout<<"Dice 6 rolled: "<<f<<endl;
-    cout<<"Dice 7 rolled: "<<g<<endl;
-    cout<<"Dice 8 rolled: "<<h<<endl;
-    cout<<"Dice 9 rolled: "<<j<<endl<<endl;
+void display_roll(vector<int>die);
+void display_roll(vector<int>die){
+    for(int i=0;i<die.size();++i){
+        cout<<"Dice: "<<i+1<<" rolled "<<die[i]<<endl;
+    }
 }
 
-//check to see if the dice number matches round number
-bool get_match(int dice, int nround);
-bool get_match(int dice, int nround){
-    bool result;
-    if(dice==nround){
-        result=true;
-    } else{
-        result=false;
-    }
-    return result;
-}
 
 //if the die matches the round number give a point
-int get_points(bool results);
-int get_points(bool results){
+int get_points(vector<int>die,int round);
+int get_points(vector<int>die,int round){
     int point;
-    if(results==true){
-        point=1;
-    }else{
-        point=0;
+    for(int i=0;i<9;++i){
+        if(die[i]==round){
+            point++;
+        }
     }
     return point;
 }
 
+//display the total games, points earned, and current round
 void display_results(int points,int games, int round);
 void display_results(int points,int games, int round){
     cout<<"Your current number of points: "<<points<<endl;
@@ -103,15 +121,10 @@ int main(int argc, char** argv) {
     int numPlay;//number of players
     int rounds;
     string again;
-    int die1;
-    int die2;
-    int die3;
-    int die4;
-    int die5;
-    int die6;
-    int die7;
-    int die8;
-    int die9;
+    vector<int>die;
+    vector<int>ai1;
+    vector<int>ai2;
+    vector<int>ai3;
     int wins;
     int losses;
     int points;
@@ -124,9 +137,21 @@ int main(int argc, char** argv) {
     //initialize variables
     points=0;
     rounds=0;
+    gplayed=0;
     
     //explain the rules but only once at the beginning of each game
     explain_rules();
+    
+    //ask the player how many people they are playing with
+    cout<<"Do you want to play this game against AI?"<<endl;
+    cout<<"Enter in the number of players to play against(up to 3)"<<endl;
+    cin>>numPlay;
+    
+    //make sure that the number entered works
+    while(numPlay<0||numPlay>3){
+        cout<<"Please enter in a valid number of players to play against"<<endl;
+        cin>>numPlay;
+    }
     
     //ask the user if they wish to start
     cout<<"Enter '1' to start the program"<<endl;
@@ -138,51 +163,14 @@ int main(int argc, char** argv) {
         //at the start of each round increase the round number
         rounds++;
         
-        //setup srand
-        srand(time(NULL));
-        
         //roll the dice
-        die1=roll_the_dice();
-        die2=roll_the_dice();
-        die3=roll_the_dice();
-        die4=roll_the_dice();
-        die5=roll_the_dice();
-        die6=roll_the_dice();
-        die7=roll_the_dice();
-        die8=roll_the_dice();
-        die9=roll_the_dice();
+        die=roll_the_dice();
         
         //let the player know what numbers they rolled
-        display_roll(die1,die2,die3,die4,die5,die6,die7,die8,die9);
+        display_roll(die);
         
         //check to see if die matches round number
-        //dice 1
-        bdie=get_match(die1,rounds);
-        points+=get_points(bdie);
-        //dice 2
-        bdie=get_match(die2,rounds);
-        points+=get_points(bdie);
-        //die 3
-        bdie=get_match(die3,rounds);
-        points+=get_points(bdie);
-        //die 4
-        bdie=get_match(die4,rounds);
-        points+=get_points(bdie);
-        //die 5
-        bdie=get_match(die5,rounds);
-        points+=get_points(bdie);
-        //die 6
-        bdie=get_match(die6,rounds);
-        points+=get_points(bdie);
-        //die 7
-        bdie=get_match(die7,rounds);
-        points+=get_points(bdie);
-        //die 8
-        bdie=get_match(die8,rounds);
-       points+=get_points(bdie);
-        //die 9
-        bdie=get_match(die9,rounds);
-        points+=get_points(bdie);
+        points=get_points(die,rounds);
         
         //at the end of the game add 1 to the number of games played
         gplayed++;
@@ -195,10 +183,11 @@ int main(int argc, char** argv) {
         write_to_file(save,points,gplayed);
         
         //ask the user if they would like to re run the program
-        cout<<"If you wish to play again enter in 'yes'"<<endl;
-        cin>>again;
+        cout<<"Do you wish to play again(y/n)"<<endl;
+        getline(cin,again);
+        cin.ignore();
         //confirm that the user intended to quit
-        if(again!="yes"||again!="Yes"){
+        if(again!="y"||again!="Y"){
             cout<<"Are you sure you want to quit?"<<endl;
             cout<<"If so type 'Q' or 'q' to quit"<<endl;
             cin>>RuSure;
@@ -207,7 +196,10 @@ int main(int argc, char** argv) {
         if(RuSure=='Q'||RuSure=='q'){
             again='n';
         }
-    } while(again=="yes"||again=="Yes");
+        else{
+            again="y";
+        }
+    } while(again=="y"||again=="Y");
     }
     return 0;
 }
